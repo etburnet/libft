c9cd27d412b7ef2576daf8b1b9afc9cd90c51676
ELBethan <monpseudojeux@gmail.com>
2023-10-24 15:15:13 +0200

    norminette on strnequ and adding strsub

--- ft_strsub.c
+++ ft_strsub.c
@@ -1,39 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnequ.c                                       :+:      :+:    :+:   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eburnet <eburnet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/24 11:58:47 by eburnet           #+#    #+#             */
-/*   Updated: 2023/10/24 15:14:47 by eburnet          ###   ########.fr       */
+/*   Created: 2023/10/24 14:58:41 by eburnet           #+#    #+#             */
+/*   Updated: 2023/10/24 15:14:50 by eburnet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stdlib.h>
 
-int	ft_strnequ(char const *s1, char const *s2, size_t n)
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
+	char	*result;
 	size_t	i;
 
 	i = 0;
-	while ((i < n || *s1) && *s1 == *s2)
+	result = NULL;
+	result = malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+	while (i < len)
 	{
-		s1++;
-		s2++;
+		result[i] = s[start + i];
 		i++;
 	}
-	if (s1[i] == s2[i])
-		return (1);
-	else
-		return (0);
+	result[i] = '\0';
+	return (result);
 }
 
 /* #include <stdio.h>
 int	main(void)
 {
-	char	*str_a = "Salut gabin";
-	char	*str_b = "Salut ethan";
-
-	printf("%d", ft_strnequ(str_a, str_b, 5));
+	char str[] = "Salut cv ou quoi ?";
+	printf("%s", ft_strsub(str, 25, 2));
 } */
